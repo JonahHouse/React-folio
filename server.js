39bdb4f65ef7f3505c898bad4095657860f73df9
Jonah Householder <jonahhouseholder@outlook.com>
2020-07-06 18:27:36 -0700

    Setting Config Vars for Heroku

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express')
 const { join } = require('path')
-// const bcrypt = require('bcrypt')
 const passport = require('passport')
 const cors = require('cors')
 const { Strategy } = require('passport-local')
@@ -10,7 +9,7 @@ const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt')
 const app = express()
 const { User } = require('./models')
 
-// app.use(express.static(join(__dirname, 'client', 'build')))
+app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -31,11 +30,11 @@ passport.use(new JWTStrategy({
 
 app.use(require('./routes'))
 
-// app.get('*', (req, res) => {
-//   res.sendFile(join(__dirname, 'client', 'build', 'index.html'))
-// })
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, 'client', 'build', 'index.html'))
+})
 
-require('mongoose').connect('mongodb://localhost/cms_db', {
+require('mongoose').connect(process.env.MONGODB_URI || process.env.LOCAL_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
